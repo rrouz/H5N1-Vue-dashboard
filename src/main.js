670bf31fda4717b13cf0f4b5b5a2be0ddd80e3bb
rrouz <ryinrouz94@gmail.com>
2025-03-19 18:48:38 -0700

    Added host view

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import IsolationSourceView from './views/IsolationSourceView.vue'
+import HostView from './views/HostSourceView.vue'  // Import the new component
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/isolations',
       name: 'isolations',
       component: IsolationSourceView
+    },
+    {
+      path: '/hosts',  // Add the new route
+      name: 'hosts',
+      component: HostView
     }
   ]
 })
